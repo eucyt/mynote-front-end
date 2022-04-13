7d35f6ace13b50c1c88b69e1a4c421fe5b1908d2
yuta <eypc.ofun@gmail.com>
2022-04-13 11:30:31 +0900

    setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.tsx'],
+    darkMode: 'media',
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+    plugins: [require('@tailwindcss/forms')],
 }
