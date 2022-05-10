2943856a3af302f70da62404264d3855015477cb
yuta <eypc.ofun@gmail.com>
2022-05-10 22:22:29 +0900

    save note

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -1,41 +1,37 @@
-import React from "react";
+import React, {memo} from "react";
 import '@toast-ui/editor/dist/toastui-editor.css';
 import {Editor} from "@toast-ui/react-editor";
 
 type Props = {
     initialValue: string
-    onChange: (value: string) => void
+    setFunc: (value: string) => void
+    ref: React.RefObject<Editor>
 }
 
 
-const MarkdownEditor: React.VFC<Props> = (props) => {
+const MarkdownEditor: React.VFC<Props> = memo((props) => {
     const editorRef = React.useRef<Editor>(null)
-
-    // const Editor = dynamic(() =>
-    //         require('@toast-ui/react-editor')
-    //     , {
-    //         ssr: false
-    //     })
 
     return (
         <Editor
             ref={editorRef}
-            // TODO: display initialed body
             initialValue={props.initialValue}
             previewStyle="vertical"
             height="600px"
             initialEditType="markdown"
             useCommandShortcut={true}
-            onChange={
+            autofocus={false}
+            // TODO: cursor jumping happened in onChange because this component is rendered
+            onBlur={
                 () => {
                     const editorInstance = editorRef.current?.getInstance()
                     if (editorInstance) {
-                        props.onChange(editorInstance?.getMarkdown())
+                        props.setFunc(editorInstance?.getMarkdown())
                     }
                 }
             }
         />
     )
-}
+})
 
 export default MarkdownEditor
--- src/pages/notes/[id].tsx
+++ src/pages/notes/[id].tsx
@@ -1,15 +1,19 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {useRouter} from "next/router";
 import {notesApi} from "@/hooks/notesApi";
 import '@toast-ui/editor/dist/toastui-editor.css';
-import MarkdownEditor from "@/components/MarkdownEditor";
+import dynamic from "next/dynamic";
+import {Editor} from "@toast-ui/react-editor";
 
 
 const Note = () => {
     const router = useRouter()
     const {fetchNote, updateNote, deleteNote, note, setNote} = notesApi()
+    const MarkdownEditor = dynamic(() => import('@/components/MarkdownEditor'), {ssr: false})
+    const [saveMessage, setSaveMessage] = useState<string>("")
+    const editorRef = React.useRef<Editor>(null)
 
     useEffect(() => {
             if (router.isReady) {
@@ -21,8 +25,7 @@ const Note = () => {
     useEffect(() => {
         if (note) {
             updateNote(note).then(() => {
-                // TODO: display message "updated"
-                console.log("updated")
+                setSaveMessage(new Date().toLocaleTimeString() + "に保存済み")
             })
         }
     }, [note])
@@ -36,7 +39,14 @@ const Note = () => {
 
             <div
                 className="m-2 sm:m-6 max-w-7xl p-6 overflow-hidden shadow-sm rounded-lg bg-white border border-gray-200">
+                <p>{saveMessage}</p>
                 <div className="relative h-10">
+                    <label className="text-xl pl-2 absolute bottom-0 inline-block">Title</label>
+                    <button
+                        className="px-2 py-1 absolute right-20 rounded transition duration-500 hover:bg-blue-400 border border-blue-200 bg-blue-100">
+                        Save
+                        {/* HACK: This is dummy. This note with latest body is saved by clicking this button because MarkdownEditor must be blur in this case. */}
+                    </button>
                     <button
                         className="px-2 py-1 absolute right-0 rounded transition duration-500 hover:bg-red-400 border border-red-200 bg-red-100"
                         onClick={() => {
@@ -45,7 +55,6 @@ const Note = () => {
                             })
                         }}>Delete
                     </button>
-                    <label className="text-xl pl-2 absolute bottom-0 inline-block">Title</label>
                 </div>
 
                 <input type="text" className="mb-6 w-full rounded border border-gray-200"
@@ -57,8 +66,9 @@ const Note = () => {
 
                 <label className="text-xl ml-2">Body</label>
                 <MarkdownEditor
+                    ref={editorRef}
                     initialValue={note?.body ?? ""}
-                    onChange={(body) => note && setNote({...note, body: body})}
+                    setFunc={(body) => note && setNote({...note, body: body})}
                 />
             </div>
         </AppLayout>
