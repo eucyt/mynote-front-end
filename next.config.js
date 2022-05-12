cfde81b9b8ba16604f5830c2a5c51a9a7c193bf8
yuta <eypc.ofun@gmail.com>
2022-05-12 13:14:09 +0900

    fix setting base path /mynote-front-end

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: process.env.GITHUB_PAGES ? '/mynote-front-end' : '',
+    trailingSlash: true,
+    assetPrefix: process.env.BASE_PATH || '',
+    basePath: process.env.BASE_PATH || '',
 }
 
 module.exports = nextConfig
