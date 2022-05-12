0e9c50a56a7026cbdc2aee0977c9c0b0518a27a3
yuta <eypc.ofun@gmail.com>
2022-05-12 13:00:48 +0900

    set base path /mynote-front-end

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    assetPrefix: process.env.GITHUB_PAGES ? '/mynote-front-end' : '',
 }
 
 module.exports = nextConfig
