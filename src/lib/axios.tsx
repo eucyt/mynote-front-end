f982c2071f7f1401e726ac3d84b1cafff39bac3c
yuta <eypc.ofun@gmail.com>
2022-05-12 16:58:05 +0900

    fix axios

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -4,6 +4,8 @@ const axios: AxiosInstance = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
     },
     withCredentials: true,
 })
