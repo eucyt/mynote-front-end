544cdc7fed30b198b4a43e72a61a47fa8c0c39a3
yuta <eypc.ofun@gmail.com>
2022-08-25 23:25:31 +0900

    fix header

--- src/components/Common/Header/Public/Header.tsx
+++ src/components/Common/Header/Public/Header.tsx
@@ -1,12 +1,8 @@
 import ApplicationLogo from '@/components/Common/ApplicationLogo'
 import Link from 'next/link'
-import NavLink from '@/components/Common/Header/NavLink'
-import { useRouter } from 'next/router'
 import React from 'react'
 
 const Header: React.VFC = () => {
-  const router = useRouter()
-
   return (
     <nav className="bg-white border-b border-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -15,29 +11,14 @@ const Header: React.VFC = () => {
             <div className="flex-shrink-0 flex items-center">
               <Link href="/">
                 <a>
-                  <ApplicationLogo className="block h-10 w-auto fill-current" />
+                  <ApplicationLogo className="block h-10 w-auto fill-current"/>
                 </a>
               </Link>
             </div>
-
-            <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-              <NavLink href="/" active={router.pathname === '/'}>
-                Home
-              </NavLink>
-            </div>
-
-            <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-              <NavLink href="/login/" active={router.pathname === '/login'}>
-                Login
-              </NavLink>
-            </div>
-
-            <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-              <NavLink
-                href="/register/"
-                active={router.pathname === '/register'}>
-                Sign up
-              </NavLink>
+            <div className="flex items-center ml-4">
+              <p className="text-sm font-medium">
+                MyNote
+              </p>
             </div>
           </div>
         </div>
