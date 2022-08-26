6ecf01f887d9c457555aaba8f2c35845e4ae7882
yuta <eypc.ofun@gmail.com>
2022-08-27 00:25:14 +0900

    add published note date

--- src/components/Notes/PublishedNoteContent.tsx
+++ src/components/Notes/PublishedNoteContent.tsx
@@ -14,7 +14,23 @@ const PublishedNoteContent: React.VFC<Props> = (props: Props) => {
         <title>{props.note.title}</title>
       </Head>
       <article className="prose">
-        <h1 className="text-3xl mb-20">{props.note.title}</h1>
+        <h1 className="text-3xl">{props.note.title}</h1>
+        <div className="flex justify-end  mb-10">
+          <p>
+            公開日:
+            <time dateTime={props.note.published_at} itemProp="datepublished">
+              {props.note.published_at.substring(0, 10)}
+            </time>
+          </p>
+          <p className="ml-4">
+            更新日:
+            <time
+              dateTime={props.note.updated_at.substring(0, 19)}
+              itemProp="modified">
+              {props.note.updated_at.substring(0, 10)}
+            </time>
+          </p>
+        </div>
         <div
           dangerouslySetInnerHTML={{ __html: marked(props.note.body ?? '') }}
         />
