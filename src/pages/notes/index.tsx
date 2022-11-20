1666d1d04675ad03a1674a75bf5311622f351c55
yuta <eypc.ofun@gmail.com>
2022-11-20 22:52:52 +0900

    fix width #11

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -34,28 +34,30 @@ const Index = () => {
           <title>MyNote - Notes</title>
         </Head>
 
-        <div className="m-2 sm:m-6 max-w-7xl p-6 overflow-hidden shadow-sm rounded-lg bg-white border border-gray-200">
-          <button
-            className="p-3 m-6 rounded transition duration-500 hover:opacity-50 border border-gray-200 bg-gray-100"
-            onClick={() => {
-              setLoading(true)
-              createNote(setNewNote)
-            }}>
-            create new note
-          </button>
+        <div className="flex justify-center">
+          <div className="m-2 sm:m-6 max-w-6xl w-full  p-6 overflow-hidden shadow-sm rounded-lg bg-white border border-gray-200">
+            <button
+              className="p-3 m-6 rounded transition duration-500 hover:opacity-50 border border-gray-200 bg-gray-100"
+              onClick={() => {
+                setLoading(true)
+                createNote(setNewNote)
+              }}>
+              create new note
+            </button>
 
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 mt-6">
-            {notes.map((note) => {
-              return (
-                <Link href={'/notes/' + note.id} key={'note' + note.id}>
-                  <div className="p-7 mx-6 mb-6 rounded overflow-hidden shadow-lg transition duration-500 hover:opacity-50 cursor-pointer border border-gray-200 h-32">
-                    <p className="overflow-hidden h-full line-clamp-3">
-                      {note.title}
-                    </p>
-                  </div>
-                </Link>
-              )
-            })}
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 mt-6">
+              {notes.map((note) => {
+                return (
+                  <Link href={'/notes/' + note.id} key={'note' + note.id}>
+                    <div className="p-7 mx-6 mb-6 rounded overflow-hidden shadow-lg transition duration-500 hover:opacity-50 cursor-pointer border border-gray-200 h-32">
+                      <p className="overflow-hidden h-full line-clamp-3">
+                        {note.title}
+                      </p>
+                    </div>
+                  </Link>
+                )
+              })}
+            </div>
           </div>
         </div>
       </AuthenticatedLayout>
